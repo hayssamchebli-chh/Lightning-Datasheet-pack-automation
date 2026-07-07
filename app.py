121c0445271c740fda6dd36d930be0d72af20aca
hayssamchebli-chh <hayssam.chebli@chhholding.com>
2026-07-07 12:28:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -180,7 +180,7 @@ def extract_names_from_text(text: str) -> list[str]:
     if not text:
         return []
 
-    raw_items = re.split(r"[\n,;\t]+", text)
+    raw_items = re.split(r"[\n;\t]+", text)
     names = []
 
     for item in raw_items:
