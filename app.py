0707aa5ad0c770e28da60d07eb7aa654e549db71
hayssamchebli-chh <hayssam.chebli@chhholding.com>
2026-07-07 16:08:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1749,7 +1749,7 @@ with right_col:
             df_preview = pd.read_excel(uploaded_file)
 
             st.caption("Excel preview")
-            st.dataframe(df_preview.head(), use_container_width=False)
+            st.dataframe(df_preview.head())
 
             excel_items, excel_error = extract_items_from_excel(uploaded_file)
             if excel_error:
